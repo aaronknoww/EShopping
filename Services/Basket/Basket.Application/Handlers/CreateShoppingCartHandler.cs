0783bf9b4a263cb31262f261b1e32557290d458e
aaronknoww <58243426+aaronknoww@users.noreply.github.com>
2024-10-08 05:29:33 +0000

    Discount project is completed and client in basket application was created

--- Services/Basket/Basket.Application/Handlers/CreateShoppingCartHandler.cs
+++ Services/Basket/Basket.Application/Handlers/CreateShoppingCartHandler.cs
@@ -1,5 +1,6 @@
 using System;
 using Basket.Application.Commands;
+using Basket.Application.GrpcService;
 using Basket.Application.Mappers;
 using Basket.Application.ResponsesDto;
 using Basket.Core.Entities;
@@ -11,13 +12,20 @@ namespace Basket.Application.Handlers;
 public class CreateShoppingCartHandler : IRequestHandler<CreateShoppingCartCommand, ShoppingCartResponse>
 {
     private readonly IBasketRepository _basketRepository;
+    private readonly DiscountGrpcService _discountGrpcService;
 
-    public CreateShoppingCartHandler(IBasketRepository basketRepository)
+    public CreateShoppingCartHandler(IBasketRepository basketRepository, DiscountGrpcService discountGrpcService)
     {
         this._basketRepository = basketRepository;
+        this._discountGrpcService = discountGrpcService;
     }
     public async Task<ShoppingCartResponse> Handle(CreateShoppingCartCommand request, CancellationToken cancellationToken)
     {
+        foreach (var item in request.Items)
+        {
+            var coupon = await _discountGrpcService.GetDiscount(item.ProductName);
+            item.Price -= coupon.Amount;
+        }
         var shoppingCart = await _basketRepository.UpdateBasket(new ShoppingCart
         {
             UserName = request.UserName,            
