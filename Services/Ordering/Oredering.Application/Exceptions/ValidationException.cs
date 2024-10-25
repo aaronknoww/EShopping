dc8b3399da6d4b0e08bbfd755e9be514ba71dfee
aaronknoww <58243426+aaronknoww@users.noreply.github.com>
2024-10-25 05:21:57 +0000

    Validation Exception was created in Ordering.Application

---
+++ Services/Ordering/Oredering.Application/Exceptions/ValidationException.cs
@@ -0,0 +1,20 @@
+using System;
+using FluentValidation.Results;
+
+namespace Ordering.Application.Exceptions;
+
+public class ValidationException : ApplicationException
+{
+    public Dictionary<string, string[]> Error { get; private set; }
+    public ValidationException():base("One or more validation error(s) occurred")
+    {
+        Error = new Dictionary<string, string[]>();
+    }
+    public ValidationException(IEnumerable<ValidationFailure> failures) : this()
+    {
+        Error = failures.GroupBy(e => e.PropertyName, e => e.ErrorMessage)
+                        .ToDictionary(failures => failures.Key, failures => failures.ToArray());
+        
+    }
+
+}
