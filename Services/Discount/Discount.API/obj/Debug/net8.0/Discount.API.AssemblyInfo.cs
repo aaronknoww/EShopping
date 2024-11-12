009ed1e3c0b60acb2c0e4f57692610595118be5f
aaronknoww <58243426+aaronknoww@users.noreply.github.com>
2024-11-12 05:55:13 +0000

    DbExtensions was implemented

--- Services/Discount/Discount.API/obj/Debug/net8.0/Discount.API.AssemblyInfo.cs
+++ Services/Discount/Discount.API/obj/Debug/net8.0/Discount.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Discount.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47d89faca1588cf6f16df099ecbbc6b6becdf5f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db8c059078d656b13d489a36a746584ff4d42667")]
 [assembly: System.Reflection.AssemblyProductAttribute("Discount.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Discount.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
