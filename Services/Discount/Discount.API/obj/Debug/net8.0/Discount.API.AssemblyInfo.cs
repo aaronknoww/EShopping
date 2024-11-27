0e09bcdab021fe60096141f706efaec5717f8474
aaronknoww <58243426+aaronknoww@users.noreply.github.com>
2024-11-27 05:57:55 +0000

    Common.Loggin was created and all reference from the projects in servies were added

--- Services/Discount/Discount.API/obj/Debug/net8.0/Discount.API.AssemblyInfo.cs
+++ Services/Discount/Discount.API/obj/Debug/net8.0/Discount.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Discount.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1e5e408373e61165cdfaeefea901a94ac17824e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+125ffd6ca92d74c2eab42c1c0b58b0ba9315fce4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Discount.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Discount.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
