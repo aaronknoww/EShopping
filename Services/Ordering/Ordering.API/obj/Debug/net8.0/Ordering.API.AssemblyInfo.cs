4c978d081c62cadb132e96fce8ca86a87d392831
aaronknoww <58243426+aaronknoww@users.noreply.github.com>
2024-11-27 06:26:51 +0000

    All sirilogs packages were added to Common.Loggin

--- Services/Ordering/Ordering.API/obj/Debug/net8.0/Ordering.API.AssemblyInfo.cs
+++ Services/Ordering/Ordering.API/obj/Debug/net8.0/Ordering.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ordering.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+125ffd6ca92d74c2eab42c1c0b58b0ba9315fce4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e09bcdab021fe60096141f706efaec5717f8474")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ordering.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ordering.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
